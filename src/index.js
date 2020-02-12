77d6474720c99d5ebec387ac9854f371c025ec52
NikeshSRaj <nikeshsraj@gmail.com>
2020-02-12 18:11:46 -0500

    Moved the state to Board component

--- src/index.js
+++ src/index.js
@@ -3,18 +3,35 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
+
     render() {
       return (
-        <button className="square">
-          {/* TODO */}
+        <button className="square" 
+                onClick={() => { this.props.onClick()}}>
+          {this.props.value}
         </button>
       );
     }
   }
   
   class Board extends React.Component {
+    constructor(props){
+      super(props);
+      this.state = {
+        squares: Array(9).fill(null)
+      };
+    }
+
     renderSquare(i) {
-      return <Square />;
+      return <Square value={this.state.squares[i]}
+                      onClick={() => this.handleClick(i)} />;
+    }
+
+    handleClick = (idx) => {
+      const squares = this.state.squares.slice();
+      squares[idx] = 'X';
+      this.setState({squares: squares});
+      console.log(this.state.squares);
     }
   
     render() {
